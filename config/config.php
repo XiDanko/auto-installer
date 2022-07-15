e6a6339b287161101c26258a8f19b24066e9bd68
Jalal Al-Mussawi <xidanko07@gmail.com>
2022-07-15 15:36:19 +0300

    create database for mysql and sqlserver

--- config/config.php
+++ config/config.php
@@ -3,15 +3,15 @@
 return [
     'service_startup_delay' => 30000,
     'commands' => [
+        ['name' => 'app:generate-autoloader',       'args' => []],
         ['name' => 'optimize:clear',                'args' => []],
         ['name' => 'app:move-to-production',        'args' => []],
-        ['name' => 'app:generate-autoloader',       'args' => []],
         ['name' => 'app:create-database',           'args' => []],
         ['name' => 'migrate:fresh',                 'args' => ['--seed' => true]],
         ['name' => 'storage:link',                  'args' => []],
         ['name' => 'app:set-as-default-site',       'args' => []],
-        ['name' => 'optimize',                      'args' => []],
         ['name' => 'app:create-websockets-service', 'args' => []],
+        ['name' => 'optimize',                      'args' => []],
         ['name' => 'app:restart-host',              'args' => []],
     ]
 ];
--- src/Console/Commands/CreateDatabase.php
+++ src/Console/Commands/CreateDatabase.php
@@ -9,21 +9,42 @@ class CreateDatabase extends Command
     protected $signature = 'app:create-database';
 
     protected $description = 'Create application database';
+    private $host;
+    private $port;
+    private $username;
+    private $password;
+    private $database;
 
     public function __construct()
     {
         parent::__construct();
+        $this->host = env('DB_HOST');
+        $this->port = env('DB_PORT');
+        $this->username = env('DB_USERNAME');
+        $this->password = env('DB_PASSWORD');
+        $this->database = env('DB_DATABASE');
     }
 
     public function handle()
     {
         try {
             $this->info('Creating database...');
-            $mysql = new \mysqli(env('db_host') . ':' . env('db_port'), env('db_username'), env('db_password'));
-            $mysql->query('CREATE DATABASE ' . env('db_database') . ' CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci');
+            if (env('DB_CONNECTION') === 'mysql') $this->createMysqlDatabase();
+            else if (env('DB_CONNECTION') === 'sqlsrv') $this->createSqlServerDatabase();
             $this->info('Database created successfully.');
         } catch (\Exception $exception) {
             $this->error($exception->getMessage());
         }
+    }
+
+    private function createMysqlDatabase()
+    {
+        $mysql = new \mysqli("$this->host:$this->port", $this->username, $this->password);
+        $mysql->query("CREATE DATABASE $this->database CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci");
+    }
+
+    private function createSqlServerDatabase()
+    {
+        exec("sqlcmd -S tcp:$this->host,$this->port -U \"$this->username\" -P \"$this->password\" -Q \"CREATE DATABASE $this->database\"");
     }
 }
